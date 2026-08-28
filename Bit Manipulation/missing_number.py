aa5706c99fda771bdfebdeea025ad32e1a63ed2a
rohan9932 <rohanrahim024@gmail.com>
2026-08-28 11:07:14 +0600

    Added solving intuition in Missing Number

--- Bit Manipulation/missing_number.py
+++ Bit Manipulation/missing_number.py
@@ -4,17 +4,21 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # Sum of n natural numbers is (n(n+1))/2
+        # If we find the difference between the expected sum and the actual sum, we get the missing number
         n = len(nums)
         exp_sum = (n*(n+1))//2
         act_sum = sum(nums)
         return exp_sum - act_sum
 
     def missingNumberXOR(self, nums: List[int]) -> int:
+        # a XOR a == 0. So if we xor all the numbers with their corresponding indices, the result will be the missing number
         ans = 0
 
         for i in range(len(nums)):
             ans ^= i ^ nums[i]
 
+        # xor with n as well
         ans ^= len(nums)
 
         return ans
